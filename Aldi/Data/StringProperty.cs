5302641f3a3c3cfcd2be190db33a933a426e906c
Jodn <jodn@posteo.de>
2018-02-12 18:02:45 +0100

    init

---
+++ Aldi/Data/StringProperty.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aldi
+{
+    public class StringProperty
+    {
+        public List<string> test1 { get; set; } = new List<string>() { "hello World","1234"};
+        public List<string> test2 { get; set; } = new List<string>();
+    }
+}
